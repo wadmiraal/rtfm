e2072227c8b286f66404a0c4ecb8a45d105f09c2
Wouter Admiraal <wad@wadmiraal.net>
2014-05-28 14:41:43 +0200

    Define base plugin for italics.

---
+++ spec/rtfm-italic.js
@@ -0,0 +1,100 @@
+
+var italic = require( '../lib/rtfm-italic' );
+
+describe( 'rtfm italic plugin', function() {
+
+    describe( 'when loaded', function() {
+
+        it( 'it is defined', function() {
+            expect( italic ).toBeDefined();
+        });
+
+        it( 'can correctly be instanciated', function() {
+            var plugin = new italic();
+            expect( plugin.register ).toBeDefined();
+        });
+
+    });
+
+    describe( 'when registering a string', function() {
+
+        it( 'ignores strings it should not handle', function() {
+            var plugin = new italic();
+            var testStrings = [
+                '098/!!po/\\/idsa',
+                '$èé$\\//àa879/8dsa!',
+                '?!!!!/!! sa/lkdjla sad',
+            ];
+            for ( var i = 0, len = testStrings.length; i < len; ++i ) {
+                expect( plugin.register( testStrings[ i ] ) ).toEqual( false );
+            }
+        });
+
+        it( 'registers strings it should handle', function() {
+            var plugin = new italic();
+            var testStrings = {
+                '!!po//id//sa': [{
+                    string: '!!po',
+                    register: false
+                }, {
+                    string: 'id',
+                    register: true
+                }, {
+                    string: 'sa',
+                    register: false
+                }],
+                '!$//èé//$àa//879//8dsa!': [{
+                    string: '!$',
+                    register: false
+                }, {
+                    string: 'èé',
+                    register: true
+                }, {
+                    string: '$àa',
+                    register: false
+                }, {
+                    string: '879',
+                    register: true
+                }, {
+                    string: '8dsa!',
+                    register: false
+                }],
+                '!!!!//!! sa\\//lk//djla sad': [{
+                    string: '!!!!',
+                    register: false
+                }, {
+                    string: '!! sa//lk',
+                    register: true
+                }, {
+                    string: 'djla sad',
+                    register: false
+                }],
+                '  !!!!!s//adsa//d!!!!': [{
+                    string: '  !!!!!s',
+                    register: false
+                }, {
+                    string: 'adsa',
+                    register: true
+                }, {
+                    string: 'd!!!!',
+                    register: false
+                }],
+                '!!!//!//!!!sadas\\//das': [{
+                    string: '!!!',
+                    register: false
+                }, {
+                    string: '!',
+                    register: true
+                }, {
+                    string: '!!!sadas//das',
+                    register: false
+                }],
+            };
+            for ( var string in testStrings ) {
+                expect( plugin.register( string ) ).toEqual( testStrings[ string ] );
+            }
+        });
+
+    });
+
+});
