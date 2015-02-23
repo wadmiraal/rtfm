9f11b13de3066d97ccfd9c391ae23ae33677190f
Wouter Admiraal <wad@wadmiraal.net>
2015-02-23 20:06:59 +0100

    Add logic for inline code blocks.

---
+++ spec/rtfm-code.js
@@ -0,0 +1,121 @@
+
+var code = require( '../lib/rtfm-code' );
+
+describe( 'rtfm code plugin', function() {
+
+    describe( 'when loaded', function() {
+
+        it( 'it is defined', function() {
+            expect( code ).toBeDefined();
+        });
+
+        it( 'can correctly be instanciated', function() {
+            var plugin = new code();
+            expect( plugin.register ).toBeDefined();
+        });
+
+    });
+
+    describe( 'when registering a string', function() {
+
+        it( 'ignores strings it should not handle', function() {
+            var plugin = new code();
+            var testStrings = [
+                '098{!!po}idsa',
+                '$èé$\\{{àa879}8dsa!',
+                '?!!!!{!! sa}lkdjla sad',
+            ];
+            for ( var i = 0, len = testStrings.length; i < len; ++i ) {
+                expect( plugin.register( testStrings[ i ] ) ).toEqual( false );
+            }
+        });
+
+        it( 'registers strings it should handle', function() {
+            var plugin = new code();
+            var testStrings = {
+                '!!po{{id}}sa': [{
+                    string: '!!po',
+                    register: false
+                }, {
+                    string: 'id',
+                    register: true
+                }, {
+                    string: 'sa',
+                    register: false
+                }],
+                '!${{èé}}$àa{{879}}8dsa!': [{
+                    string: '!$',
+                    register: false
+                }, {
+                    string: 'èé',
+                    register: true
+                }, {
+                    string: '$àa',
+                    register: false
+                }, {
+                    string: '879',
+                    register: true
+                }, {
+                    string: '8dsa!',
+                    register: false
+                }],
+                '{{!! sa\\{{lk}}djla sad': [{
+                    string: '!! sa{{lk',
+                    register: true
+                }, {
+                    string: 'djla sad',
+                    register: false
+                }],
+                '{{!! sa\\{{lk}}': [{
+                    string: '!! sa{{lk',
+                    register: true
+                }],
+                '!!!!{{!! sa\\{{lk}}djla sad': [{
+                    string: '!!!!',
+                    register: false
+                }, {
+                    string: '!! sa{{lk',
+                    register: true
+                }, {
+                    string: 'djla sad',
+                    register: false
+                }],
+                '!!!!{{!! sa\\}}lk}}djla sad': [{
+                    string: '!!!!',
+                    register: false
+                }, {
+                    string: '!! sa}}lk',
+                    register: true
+                }, {
+                    string: 'djla sad',
+                    register: false
+                }],
+                '  !!!!!s{{adsa}}d!!!!': [{
+                    string: '  !!!!!s',
+                    register: false
+                }, {
+                    string: 'adsa',
+                    register: true
+                }, {
+                    string: 'd!!!!',
+                    register: false
+                }],
+                '!!!{{!}}!!!sadas\\{{das': [{
+                    string: '!!!',
+                    register: false
+                }, {
+                    string: '!',
+                    register: true
+                }, {
+                    string: '!!!sadas{{das',
+                    register: false
+                }],
+            };
+            for ( var string in testStrings ) {
+                expect( plugin.register( string ) ).toEqual( testStrings[ string ] );
+            }
+        });
+
+    });
+
+});
